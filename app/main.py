91ab03a5fd3fafff172decfb787c327a843fa921
Joey <j.choong@accenture.com>
2025-02-20 18:48:22 +0800

    add request_date

--- app/main.py
+++ app/main.py
@@ -3,12 +3,18 @@ from pydantic import BaseModel
 import requests
 import os
 import time
+import datetime
+import logging
 
 class SearchRequest(BaseModel):
     search_term: str
     index: str | None = None
+    latest_time: str | None = None
+    earliest_time: str | None = None
+    search_date: str | None = None
 
 app = FastAPI()
+logger = logging.getLogger('uvicorn.error')
 
 SPLUNK_HOST = os.environ.get('SPLUNK_HOST')
 SPLUNK_TOKEN = os.environ.get('SPLUNK_TOKEN')
@@ -33,12 +39,25 @@ def trigger_search(request: SearchRequest):
         if request.index is None:
             search_index = "test_index_01"
 
+        latest_time = request.latest_time or ""
+        earliest_time = request.earliest_time or "0"
+
+        if request.search_date is not None:
+            request_date_start= datetime.datetime.fromisoformat(request.search_date)
+            request_date_end = request_date_start + datetime.timedelta(days=1)
+
+            earliest_time = request_date_start.timestamp()
+            latest_time = request_date_end.timestamp()
+
+
         search_query = f"search index=\"{search_index}\" {request.search_term}"
         payload = {
             "search": search_query,
-            "latest_time": "",
-            "earliest_time": "0"
+            "latest_time": latest_time,
+            "earliest_time": earliest_time
         }
+
+        logger.info(f"Search payload: {payload}")
         
         response = requests.post(API_URL, data=payload, headers=HEADERS, verify=False)
         response_data = response.json()
@@ -46,7 +65,7 @@ def trigger_search(request: SearchRequest):
         if response.status_code == 201 and "sid" in response_data:
             sid = response_data["sid"]
 
-            print(f"Search job created with SID: {sid}")
+            logger.info(f"Search job created with SID: {sid}")
 
             # Poll for search job status
             for _ in range(MAX_RETRIES):
@@ -54,12 +73,12 @@ def trigger_search(request: SearchRequest):
                 if status_response.status_code == 200:
                     status_data = status_response.json()
                     dispatch_state = status_data.get("entry", [{}])[0].get("content", {}).get("dispatchState", "")
-                    print(f"Search job status: {dispatch_state}")
+                    logger.info(f"Search job status: {dispatch_state}")
                     
                     if dispatch_state == "DONE":
                         break
                 retryDelaySeconds = RETRY_DELAY * _;
-                print(f"Retrying in {retryDelaySeconds} seconds")
+                logger.info(f"Retrying in {retryDelaySeconds} seconds")
                 time.sleep(retryDelaySeconds)
             else:
                 raise HTTPException(status_code=408, detail="Search job did not complete in time")
@@ -67,10 +86,10 @@ def trigger_search(request: SearchRequest):
             results_response = requests.get(RESULTS_API_URL.format(sid=sid), headers=HEADERS, verify=False)
 
             if results_response.status_code == 200:
-                print(f"Success retrieving {sid}, returning results")
+                logger.info(f"Success retrieving {sid}, returning results")
                 return results_response.json()
             else:
-                print(f"Not successful retrieving {sid}, returning error: {results_response.status_code}")
+                logger.info(f"Not successful retrieving {sid}, returning error: {results_response.status_code}")
                 raise HTTPException(status_code=results_response.status_code, detail=results_response.json())
         
         raise HTTPException(status_code=response.status_code, detail=response_data)
